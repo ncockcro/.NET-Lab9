2d6dd2c7567806213a09426313cf775865b5040b
ncockcro <35208141+ncockcro@users.noreply.github.com>
2018-05-14 13:14:11 -0400

    Completed Lab 9
    Full implementation for creating a GUI where the user can enter a string and what they want to split the string on, and in the output text box will be the parsed string.

---
+++ lab9Form.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Lab9
+{
+    partial class lab9Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.patternTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stringTextBox = new System.Windows.Forms.TextBox();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // patternTextBox
+            // 
+            this.patternTextBox.Location = new System.Drawing.Point(389, 108);
+            this.patternTextBox.Name = "patternTextBox";
+            this.patternTextBox.Size = new System.Drawing.Size(818, 31);
+            this.patternTextBox.TabIndex = 0;
+            this.patternTextBox.TextChanged += new System.EventHandler(this.patternTextBox_TextChanged);
+            this.patternTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.patternTextBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(384, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pattern";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "String";
+            // 
+            // stringTextBox
+            // 
+            this.stringTextBox.Location = new System.Drawing.Point(389, 264);
+            this.stringTextBox.Name = "stringTextBox";
+            this.stringTextBox.Size = new System.Drawing.Size(818, 31);
+            this.stringTextBox.TabIndex = 3;
+            this.stringTextBox.TextChanged += new System.EventHandler(this.stringTextBox_TextChanged);
+            this.stringTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.stringTextBox_KeyDown);
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(389, 436);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ReadOnly = true;
+            this.resultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultsTextBox.Size = new System.Drawing.Size(818, 316);
+            this.resultsTextBox.TabIndex = 4;
+            this.resultsTextBox.TextChanged += new System.EventHandler(this.resultsTextBox_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(389, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Results";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lab9Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1642, 863);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.stringTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.patternTextBox);
+            this.Name = "lab9Form";
+            this.Text = "Lab 9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox patternTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox stringTextBox;
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab9Form.cs
@@ -0,0 +1,166 @@
+﻿/*
+ * Written by: Nicholas Cockcroft
+ * Date: March 20, 2018
+ * Course: .NET Environment
+ * Assignment: Lab 9
+ * 
+ * Description: Write a Split tester.  It will be a windows application with 3 text boxes. 
+ * In the first two you enter the pattern and the string to be split according to the pattern. 
+ * The last text box will be read-only with vertical scrolling which will contain the substrings
+ * into which the original string was parsed.  This lab is a great way to test your knowledge of 
+ * regular expressions and the Split method by playing with this code.  Please play with your program.  
+ * When the user hits enter in any text box, apply the split.
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions; // Regular Expressions
+using System.Collections.Generic; // Stack
+
+namespace Lab9
+{
+    public partial class lab9Form : Form
+    {
+        public lab9Form()
+        {
+            InitializeComponent();
+        }
+
+        private void patternTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void patternTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If the key is not enter, return
+            if(e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            // Otherwise, we call a fuction to perform the splitting with the pattern
+            splitText();
+        }
+
+        private void stringTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If the key is not enter, return
+            if(e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            // Otherwise, split just like in the pattern text box
+            splitText();
+        }
+
+        private void splitText()
+        {
+            // Get the pattern and string text from the different text boxes
+            string pattern = patternTextBox.Text;
+            string text = stringTextBox.Text;
+
+            // Initialize the results text box to nothing
+            resultsTextBox.Text = "";
+
+            // If there is something wrong with the pattern the user typed in,
+            // we output text to the results text box and return
+            if(validPattern(pattern) == false)
+            {
+                resultsTextBox.Text = "Error: Invalid pattern.";
+                return;
+            }
+
+            // Otherwise, we perform the split with whatever the user typed in
+            Regex regexPattern = new Regex(pattern);
+
+            string[] elts = regexPattern.Split(text);
+
+            for (int i = 0; i < elts.Length; i++)
+            {
+                resultsTextBox.Text += elts[i];
+                resultsTextBox.Text += "\r\n";
+            }
+        }
+
+        private bool validPattern(string pattern)
+        {
+            bool isValid = true;
+            Stack<char> parenthesis = new Stack<char>();
+
+            // If the pattern contains a '*' and a '+' then we return false
+            if(pattern.Contains('*') && pattern.Contains('+'))
+            {
+                return isValid = false;
+            }
+
+            // If the pattern only has a '+' or only has a '*' then we return false
+            if(pattern == "+" || pattern == "*")
+            {
+                return isValid = false;
+            }
+
+            // If the pattern has a '?' and '+' then we return false
+            if(pattern.Contains('?') && pattern.Contains('+'))
+            {
+                return isValid = false;
+            }
+
+            // If the pattern has a '?' and '*' then we return false
+            if(pattern.Contains('?') && pattern.Contains('*'))
+            {
+                return isValid = false;
+            }
+
+            // Here we check to make sure if the user types in parenthesis, that they match
+            for(int i = 0; i < pattern.Length; i++)
+            {
+                // If the user types in a closing brace first, we return false
+                if (pattern[i] == ')' || pattern[i] == ']' || pattern[i] == '}')
+                {
+                    if (parenthesis.Count == 0)
+                    {
+                        return isValid = false;
+                    }
+                    parenthesis.Pop();
+                }
+
+                if (pattern[i] == '(' || pattern[i] == '[' || pattern[i] == '{')
+                 {
+                     parenthesis.Push(pattern[i]);
+                 }
+
+             }
+            // If the stack is not empty by the end, there is an extra brace and we return false
+             if(parenthesis.Count > 0)
+             {
+                 return isValid = false;
+             }
+
+            return isValid;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultsTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stringTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
